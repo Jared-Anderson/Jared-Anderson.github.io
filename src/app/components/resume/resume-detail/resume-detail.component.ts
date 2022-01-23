e9e31b057680cc3d5bfccc668f9af78a44126f74
Jared Anderson <janderson1022@gmail.com>
2022-01-23 15:26:53 -0600

    Resume detail icon additions

--- src/app/components/resume/resume-detail/resume-detail.component.ts
+++ src/app/components/resume/resume-detail/resume-detail.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { faCalendar } from '@fortawesome/free-regular-svg-icons';
+import {
+  faBirthdayCake,
+  faLocationArrow,
+  faMapPin,
+  faMugHot,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-resume-detail',
@@ -7,8 +13,26 @@ import { faCalendar } from '@fortawesome/free-regular-svg-icons';
   styleUrls: ['./resume-detail.component.scss'],
 })
 export class ResumeDetailComponent implements OnInit {
-  briefcaseIcon = faCalendar;
+  mapMarkerIcon = faMapPin;
+  birthdayCakeIcon = faBirthdayCake;
+  mugHotIcon = faMugHot;
   constructor() {}
 
   ngOnInit(): void {}
+
+  // function to calculate age
+  calculateAge() {
+    const birthday = new Date('10/22/1994');
+    let ageDifMs = Date.now() - birthday.getTime();
+    let ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
+
+  // function to calculate years of experience
+  calculateExperience() {
+    const startDate = new Date('01/01/2018');
+    let experienceDifMs = Date.now() - startDate.getTime();
+    let experienceDate = new Date(experienceDifMs);
+    return Math.abs(experienceDate.getUTCFullYear() - 1970);
+  }
 }
